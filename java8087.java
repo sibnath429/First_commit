57ae216b15ecdb629c72bac09ef17ed4eae65b36
KoushikAni <leokoushik6@gmail.com>
2020-05-04 21:37:59 +0530

    Java

---
+++ java8087.java
@@ -0,0 +1,43 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Date;
+import java.util.Scanner;
+public class java8087 {
+    private Socket socket;
+    private Scanner scanner;
+    private java8087(InetAddress serverAddress, int serverPort) throws Exception {
+        this.socket = new Socket(serverAddress, serverPort);
+        this.scanner = new Scanner(System.in);
+    }
+    private void start() throws IOException {
+        String input;
+        while (true) {
+           input = " mirna  " + new Date().toString();
+            System.out.println("sent size " + input.getBytes().length);
+            JsonObject  jsonObject = new JsonObject();
+            jsonObject.addProperty("id",10);
+            jsonObject.addProperty("Name","8087");
+            jsonObject.addProperty("time",input);
+            PrintWriter out = new PrintWriter(this.socket.getOutputStream(),true);
+            out.println(jsonObject.toString());
+            out.flush();
+            try
+            {
+                Thread.sleep(2000);
+            }catch (InterruptedException e) {}
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        java8087 client = new java8087(
+                InetAddress.getByName("127.0.0.1"),
+                8087);
+
+        System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
+        client.start();
+    }
+}
